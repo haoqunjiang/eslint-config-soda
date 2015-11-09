0d0384304d965604c24c81bb0eed11872d0c330f
sodatea <sodazju@gmail.com>
2015-11-09 13:26:27 +0800

    Update max-params & max-nested-callbacks

--- rules/complexity.js
+++ rules/complexity.js
@@ -4,20 +4,24 @@ module.exports = {
         // 单个函数最多有 30 行（内部的其他函数定义不算）
         'max-statements': [2, 30],
 
+        // 每行最多 140 个字符，比 80 个字符多一点，毕竟可能会有很多缩进，而且反正显示器够大
+        'max-len': [1, 140],
+
+        // If a function has too many parameters, you should consider taking an `option` object as the paramter.
+        // Better turn off for AMD & Angular 1.x modules by inline comments
+        // 如果函数参数太多的话，应当使用 `option` 对象作为参数
+        // 对于 AMD 格式以及 Angular 1.x 模块，最好通过 inline comments 的方式关闭这条规则
+        'max-params': [1, 5],
+
         // 至多嵌套五层代码块
         'max-depth': [2, 5],
 
-        // 每行最多 140 个字符，比 80 个字符多一点，毕竟可能会有很多缩进，而且反正显示器够大
-        // 因为目前开发使用 AMD 模块，define 的时候依赖注入的参数列表可能有点长……于是暂时放弃这个校验
-        'max-len': 0,  // [1, 140]
-
-        // 上限设置到 10 是因为 Angular 依赖注入可能会有很多
-        'max-params': [1, 10],
-
+        // Too many nested callbacks would be hard to read
         // 最多允许嵌套两层回调
-        'max-nested-callbacks': [1, 2],
+        'max-nested-callbacks': [1, 4],
 
-        // [cyclomatic complexity](http://en.wikipedia.org/wiki/Cyclomatic_complexity) 圈复杂度上限为 7
-        'complexity': [1, 7]
+        // [cyclomatic complexity](http://en.wikipedia.org/wiki/Cyclomatic_complexity) should not be greater than 8
+        // 圈复杂度上限为 8
+        'complexity': [1, 8]
     }
 };
